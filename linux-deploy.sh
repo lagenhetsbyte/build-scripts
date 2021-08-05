decacd1354d0ff2e4a97bf0f44001e5c9ac5dbda
Henrik Matsson <henrik.mattsson@hotmail.com>
2021-08-05 17:22:39 +0200

    install node & pm2

--- linux-deploy.sh
+++ linux-deploy.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-if [ -d "./search-api" ]; then
-    echo "Directory /path/to/dir exists."
+AUTHENTICATED_REPOPATH = $1;
+PROJECTNAME = $2;
+
+if [ -d "./${PROJECTNAME}" ]; then
+    git pull ${AUTHENTICATED_REPOPATH}
 else
-    echo "Error."
+    git clone ${AUTHENTICATED_REPOPATH}
 fi
+
+cd ${PROJECTNAME}
+
+echo "Checking npm..."
+if ! type "npm" > /dev/null; then
+    echo "Installing npm..."
+    sudo apt-get update
+
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+    sudo apt-get -y install nodejs
+else
+    echo "Npm found!";
+fi
+
+echo "Checking pm2..."
+if ! type "pm2" > /dev/null; then
+  echo "Installing pm2..."
+  sudo npm install pm2 -g
+else
+  echo "PM2 found!"
+fi
+
+npm install
