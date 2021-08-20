453d12ad5126c1c05b0672149ed0b7bba1076f44
Oscar <80750422+oggeorre@users.noreply.github.com>
2021-08-20 14:02:57 +0200

    Update deploy-docker-vps.sh

--- deploy-docker-vps.sh
+++ deploy-docker-vps.sh
@@ -55,14 +55,13 @@ COMMAND_2="aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID""
 COMMAND_3="aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY""
 COMMAND_4="aws ecr get-login-password --region "$AWS_REGION" | sudo docker login --username AWS --password-stdin "$AWS_DOMAIN""
 COMMAND_5="sudo docker pull "$REPOSITORY_URI":latest"
-COMMAND_6="sudo docker stop "$CONTAINER""
-COMMAND_7="sudo docker rm "$CONTAINER" -f"
+COMMAND_7="sudo docker rm "$CONTAINER" -f || true"
 COMMAND_10="sudo docker volume create data"
 COMMAND_8="sudo docker run -d --name "$CONTAINER" "$DOCKER_RUN_COMMAND" "$REPOSITORY_URI":latest"
 COMMAND_9="sudo docker image prune -a -f"
 
 echo Connecting and running commands on remote server
-ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$VPS_USER"@"$VPS_HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3"; "$COMMAND_4"; "$COMMAND_5"; "$COMMAND_6"; "$COMMAND_7"; "$COMMAND_10"; "$COMMAND_8"; "$COMMAND_9""
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$VPS_USER"@"$VPS_HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3"; "$COMMAND_4"; "$COMMAND_5"; "$COMMAND_7"; "$COMMAND_10"; "$COMMAND_8"; "$COMMAND_9""
 
 echo All done.
 
