b4b0ce53e8848209b31d6f72627dd925acda69a2
Oscar <80750422+oggeorre@users.noreply.github.com>
2022-11-15 12:01:41 +0100

    Added clean up

--- blackbox/deploy-private.sh
+++ blackbox/deploy-private.sh
@@ -45,12 +45,7 @@ scp -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" $INSTRUCTION_FILE "$USER"@"$HO
 COMMAND_1="wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/blackbox.zip && unzip -o blackbox.zip"
 COMMAND_2="node replace_image.js "$DEPLOYMENT_INSTRUCTION_FILE" "$REPOSITORY_URI:$IMAGE_TAG""
 COMMAND_3="sudo node deploy.js "$DEPLOYMENT_INSTRUCTION_FILE""
+COMMAND_4="wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/remove-private-registry-images.sh; sudo bash ./remove-private-registry-images.sh BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="5" TAG_PREFIX="$TAG_PREFIX""
 
 echo "Connecting and running commands on remote server"
-ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$USER"@"$HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3";"
-
-echo "EXIT CODE $?"
-exitCode=$?
-if [ $exitCode -eq 0 ]; then
-    ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$USER"@"$HOST" "wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/remove-private-registry-images.sh; sudo bash ./remove-private-registry-images.sh BASE_PATH="/mnt/docker-registry-storage" REPO=$REPO KEEP=5 TAG_PREFIX=$TAG_PREFIX"
-fi
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$USER"@"$HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3"; "$COMMAND_4";"
