4ac246968a34fb7f43f12225674a15eee67765f7
Oscar <80750422+oggeorre@users.noreply.github.com>
2022-11-15 12:04:05 +0100

    Fixed name

--- blackbox/deploy-private.sh
+++ blackbox/deploy-private.sh
@@ -45,7 +45,7 @@ scp -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" $INSTRUCTION_FILE "$USER"@"$HO
 COMMAND_1="wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/blackbox.zip && unzip -o blackbox.zip"
 COMMAND_2="node replace_image.js "$DEPLOYMENT_INSTRUCTION_FILE" "$REPOSITORY_URI:$IMAGE_TAG""
 COMMAND_3="sudo node deploy.js "$DEPLOYMENT_INSTRUCTION_FILE""
-COMMAND_4="curl -L https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/deploy-private.sh | bash -s ./remove-private-registry-images.sh BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="5" TAG_PREFIX="$TAG_PREFIX""
+COMMAND_4="curl -L https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/remove-private-registry-images.sh | bash -s ./remove-private-registry-images.sh BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="5" TAG_PREFIX="$TAG_PREFIX""
 
 echo "Connecting and running commands on remote server"
 ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$USER"@"$HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3"; "$COMMAND_4";"
