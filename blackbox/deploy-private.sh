7be144e1d71eac2c229e8ef8234f0ba014d402b1
Oscar <80750422+oggeorre@users.noreply.github.com>
2022-11-16 11:42:13 +0100

    Added logging

--- blackbox/deploy-private.sh
+++ blackbox/deploy-private.sh
@@ -46,7 +46,14 @@ DEPLOYMENT_INSTRUCTION_FILE="$IMAGE_TAG-deploy-$RANDOM_STR.json"
 
 scp -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" $INSTRUCTION_FILE "$USER"@"$HOST":"$DEPLOYMENT_INSTRUCTION_FILE"
 
+echo "Download blackbox"
 ssh_command "wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/blackbox.zip && unzip -o blackbox.zip"
+
+echo "Replace image in instruction"
 ssh_command "node replace_image.js "$DEPLOYMENT_INSTRUCTION_FILE" "$REPOSITORY_URI:$IMAGE_TAG""
+
+echo "Run blackbox deployment"
 ssh_command "sudo node deploy.js "$DEPLOYMENT_INSTRUCTION_FILE""
+
+echo "Clean up repo images"
 ssh_command "curl -L https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/remove-private-registry-images.sh | bash -s BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="3" TAG_PREFIX="$TAG_PREFIX""
--- blackbox/replace_image.js
+++ blackbox/replace_image.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
 
 function replaceImages() {
-  const instructionFile = process.argv[2];
-  const instruction = JSON.parse(fs.readFileSync(instructionFile));
+  try {
+    const instructionFile = process.argv[2];
+    const instruction = JSON.parse(fs.readFileSync(instructionFile));
 
-  const image = process.argv[3];
-  console.log("Replacing service images to:", image);
+    const image = process.argv[3];
+    console.log("Replacing service images to:", image);
 
-  instruction.services.forEach((x) => (x.image = image));
+    instruction.services.forEach((x) => (x.image = image));
 
-  fs.writeFileSync(instructionFile, JSON.stringify(instruction));
+    fs.writeFileSync(instructionFile, JSON.stringify(instruction));
+  } catch (error) {
+    console.log("Failed to replace service image:", error);
+    process.exit(1);
+  }
 }
 
 replaceImages();
