ab65f80310c257fe2daba2a0e16b78e1bb3fdc3a
Oscar <80750422+oggeorre@users.noreply.github.com>
2022-11-16 11:33:40 +0100

    Run ssh commands separate to get correct exit code

--- blackbox/deploy-private.sh
+++ blackbox/deploy-private.sh
@@ -24,6 +24,10 @@ for ARGUMENT in "$@"; do
 
 done
 
+ssh_command() {
+    ssh -o StrictHostKeyChecking=no -i $SSH_KEY_DIR $USER@$HOST $1
+}
+
 IMAGE_TAG=$TAG_PREFIX$IMAGE_TAG
 REPOSITORY_URI=$REGISTRY_DOMAIN/$REPO
 
@@ -42,10 +46,7 @@ DEPLOYMENT_INSTRUCTION_FILE="$IMAGE_TAG-deploy-$RANDOM_STR.json"
 
 scp -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" $INSTRUCTION_FILE "$USER"@"$HOST":"$DEPLOYMENT_INSTRUCTION_FILE"
 
-COMMAND_1="wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/blackbox.zip && unzip -o blackbox.zip"
-COMMAND_2="node replace_image.js "$DEPLOYMENT_INSTRUCTION_FILE" "$REPOSITORY_URI:$IMAGE_TAG""
-COMMAND_3="sudo node deploy.js "$DEPLOYMENT_INSTRUCTION_FILE""
-COMMAND_4="curl -L https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/remove-private-registry-images.sh | bash -s BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="3" TAG_PREFIX="$TAG_PREFIX""
-
-echo "Connecting and running commands on remote server"
-ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_DIR" "$USER"@"$HOST" ""$COMMAND_1"; "$COMMAND_2"; "$COMMAND_3"; "$COMMAND_4";"
+ssh_command "wget -N https://github.com/lagenhetsbyte/build-scripts/raw/master/blackbox/blackbox.zip && unzip -o blackbox.zip"
+ssh_command "node replace_image.js "$DEPLOYMENT_INSTRUCTION_FILE" "$REPOSITORY_URI:$IMAGE_TAG""
+ssh_command "sudo node deploy.js "$DEPLOYMENT_INSTRUCTION_FILE""
+ssh_command "curl -L https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/remove-private-registry-images.sh | bash -s BASE_PATH="/mnt/docker-registry-storage" REPO="$REPO" KEEP="3" TAG_PREFIX="$TAG_PREFIX""
