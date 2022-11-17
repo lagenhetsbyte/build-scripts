ed8d84344a7a82b317c30cba3f0b83703e6fc316
Oscar <80750422+oggeorre@users.noreply.github.com>
2022-11-17 10:50:23 +0100

    Run as sudo

--- blackbox/install-registry-cleanup.sh
+++ blackbox/install-registry-cleanup.sh
@@ -4,10 +4,10 @@ set -e
 echo "Downloading cleanup script"
 wget -N https://raw.githubusercontent.com/lagenhetsbyte/build-scripts/master/blackbox/registry-cleanup.sh
 echo "Moving script"
-mv ./registry-cleanup.sh /mnt/docker-registry-storage/
+sudo mv ./registry-cleanup.sh /mnt/docker-registry-storage/
 
 echo "Adding job to crontab if not already there"
-grep -qxF '*/15 * * * * root /mnt/docker-registry-storage/registry-cleanup.sh' /etc/crontab || echo '*/15 * * * * root /mnt/docker-registry-storage/registry-cleanup.sh' >>/etc/crontab
+sudo grep -qxF '*/15 * * * * root /mnt/docker-registry-storage/registry-cleanup.sh' /etc/crontab || echo '*/15 * * * * root /mnt/docker-registry-storage/registry-cleanup.sh' >>/etc/crontab
 
 echo "Reloading cronjob"
 sudo service cron reload
